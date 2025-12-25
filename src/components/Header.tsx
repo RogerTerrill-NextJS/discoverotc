2296a053345ebc5141b9f9b8207a5399c9ee3972
Roger Terrill <lpaddikt@gmail.com>
2025-12-25 00:30:42 -0800

    Added logo image and logo to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from "next/link";
 import SocialMediaIcons from "@/components/SocialMediaIcons";
+import Image from "next/image";
 
 const Header = () => {
   return (
@@ -8,10 +9,7 @@ const Header = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center justify-between">
         <Link href="/" className="flex items-center cursor-pointer">
           <div className="bg-blue-600 text-white p-1.5 rounded-lg mr-3 shadow-sm">
-            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                    d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"/>
-            </svg>
+            <Image src="/logo.png" width={20} height={20} alt="Outside The Cockpit Logo" className="w-5 h-5"/>
           </div>
           <h1 className="text-xl font-bold tracking-tight text-slate-800">
             <span className="text-blue-600">Outside The Cockpit</span>
