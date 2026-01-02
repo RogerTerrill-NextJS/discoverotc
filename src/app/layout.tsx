35e38f7994e8869733255ef2b6f20779504b13fb
Roger Terrill <lpaddikt@gmail.com>
2026-01-02 02:19:40 -0800

    Temporarily removed NewsletterSignup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
-        <NewsletterSignup />
+        {/*<NewsletterSignup />*/}
         <Footer />
       </body>
     </html>
